97b2eff447d151de25d671f2d70f7a6ccc2147a3
unknown <Aprendiz@BOGDFPCGMF4122.FormacionSena.RED>
2023-08-29 17:48:23 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const path = require("path");
+
+const app = express();
+
+app.get("/", (req, res)=>{
+    //res.send("Hello World!")
+    res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+app.listen(3000, () =>{
+    console.log("server listening on port", 3000);
+});
